2266bd961a40b6f8b9e8a0d296510d70852124a3
Luís Silva <luiscomsnofim@gmail.com>
2025-01-26 16:39:50 +0100

    fix: change of height

--- src/lib/Todos.svelte
+++ src/lib/Todos.svelte
@@ -266,6 +266,32 @@
 								</div>
 
 								<div class="ml-auto flex items-center gap-2">
+									<!-- Edit button is hidden by default, shown on hover -->
+									<button
+										on:click={() => startEdit(item)}
+										class="edit-button px-2 py-1 opacity-0 group-hover:opacity-100"
+										aria-label="Edit todo"
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											class="size-6"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z"
+											/>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M4.867 19.125h.008v.008h-.008v-.008Z"
+											/>
+										</svg>
+									</button>
 									<span
 										class="text-sm"
 										class:text-red-500={new Date().setHours(0, 0, 0, 0) >
@@ -275,13 +301,6 @@
 									>
 										{new Date(item.due).toLocaleDateString('en-GB')}
 									</span>
-									<!-- Edit button is hidden by default, shown on hover -->
-									<button
-										on:click={() => startEdit(item)}
-										class="edit-button hidden rounded bg-yellow-500 px-2 py-1 text-white hover:bg-yellow-600 group-hover:inline-block"
-									>
-										Edit
-									</button>
 								</div>
 							</div>
 						{/if}
