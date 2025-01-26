cd5aeba0bdd5928c998f448463c07a924e161aa5
Luís Silva <luiscomsnofim@gmail.com>
2025-01-26 17:27:55 +0100

    fix: move notepad page to notepade page file

--- src/routes/notepad/+page.svelte
+++ src/routes/notepad/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import Notes from '$lib/Notes.svelte';
+	import Editor from '$lib/Editor.svelte';
+
+	import { LocalStorage } from '$lib/storage.svelte';
+
+	const notepad = new LocalStorage('notepad', {
+		type: 'doc',
+		content: [{ type: 'paragraph', content: [{ type: 'text', text: 'Write Your Notes Here' }] }]
+	});
+
+	function handleEditorChange(event: string) {
+		notepad.current = event;
+	}
 </script>
 
-<Notes />
+<div class="flex h-full flex-1 flex-col rounded-lg border-2 border-black bg-white p-4">
+	<Editor value={notepad.current} onchange={handleEditorChange} />
+</div>
 
 <svelte:head>
-	<title>Bouldy</title>
+	<title>Bouldy - notepad</title>
 </svelte:head>
