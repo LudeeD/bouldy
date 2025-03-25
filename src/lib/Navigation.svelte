6ed342321f49319049869666df72574506ffa55e
Luís Silva <luiscomsnofim@gmail.com>
2025-03-25 22:19:03 +0100

    fix: add pomodoro to navigation

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -6,9 +6,17 @@
 		<li>
 			<a href="/notepad" class="decoration-2 underline-offset-8 hover:underline">Notepad</a>
 		</li>
+		<li>
+			<a href="/pomodoro" class="decoration-2 underline-offset-8 hover:underline">Notepad</a>
+		</li>
 	</ul>
 	<div class="mt-4 flex justify-between text-sm text-gray-600">
 		<span>Built: {new Date(import.meta.env.BUILD_TIMESTAMP).toUTCString()}</span>
-		<span>Made with <a href="/debug">❤️</a> by <a href="https://github.com/ludeed" class="decoration-2 underline-offset-8 hover:underline">ludeed</a></span>
+		<span
+			>Made with <a href="/debug">❤️</a> by
+			<a href="https://github.com/ludeed" class="decoration-2 underline-offset-8 hover:underline"
+				>ludeed</a
+			></span
+		>
 	</div>
 </nav>
