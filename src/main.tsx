ef899b8e67e2b0d65f494862b279881ae769de00
Luís Silva <luiscomsnofim@gmail.com>
2025-11-23 11:04:47 +0100

    feat: add telemetry

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  person_profiles: "identified_only",
+  capture_pageview: false, // Disable automatic pageview capture for Tauri apps
+  capture_pageleave: false,
+  autocapture: true,
+} as const;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 );
